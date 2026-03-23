d801b036f27e53150b526412e9db72c5620faadc
ryhowww <ryan@agentvoice.com>
2026-03-23 11:55:05 -0500

    Increase Express JSON body limit to 2MB for merge-css endpoint

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { warmSite, isRunning, getAllStatus } from "./warmer.js";
 import { info } from "./logger.js";
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '2mb' }));
 
 const PORT = process.env.PORT || 3000;
 const AUTH_TOKEN = process.env.AUTH_TOKEN || null;
